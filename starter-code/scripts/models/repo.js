78f90bbdddc6059f286afc4943631748ea56ff7e
Erica Winberry <elaewin@gmail.com>
2016-07-27 13:20:39 -0700

    updates from lab.

--- starter-code/scripts/models/repo.js
+++ starter-code/scripts/models/repo.js
@@ -4,8 +4,23 @@
   reposObj.allRepos = [];
 
   reposObj.requestRepos = function(callback) {
-    /* TODO: How would you like to fetch your repos? Someone say AJAX?!
+    /* DONE-Corey: How would you like to fetch your repos? Someone say AJAX?!
       Do not forget to call the callback! */
+    $.ajax({
+      url: 'https://api.github.com/users/codefellows-seattle-301d9/repos' +
+           '?per_page=10' +
+           '&sort=update',
+      type: 'GET',
+      headers: {
+        'Authorization': 'token ' + token,
+      },
+      success: function(data, message, xhr) {
+        data.forEach(function(obj) {
+          reposObj.allRepos.push(obj);
+        });
+        callback();
+      }
+    });
   };
 
   reposObj.withTheAttribute = function(myAttr) {
--- starter-code/scripts/views/repoView.js
+++ starter-code/scripts/views/repoView.js
@@ -1,18 +1,24 @@
 (function(module) {
   var repoView = {};
-  /* TODO: Let's compile our new template!
+
+    /* DONE: Let's compile our new template!
        Save the result of invoking Handlebars in this 'repoCompiler' variable
        that we will pass to the append method below. */
-  var repoCompiler;  // Finish the Handlebars method here!
+  var repoCompiler = Handlebars.compile($('#repo-template').text());
+  // Finish the Handlebars method here!
 
   repoView.renderRepos = function() {
     $('#about ul').empty().append(
       reposObj.withTheAttribute('name')  // TODO: experiment changing this attribute!
       .map(repoCompiler)
     );
+    console.log('appending has occurred!');
   };
-/* TODO: Call the function that loads (or 'requests') our repo data.
+/* DONE: Call the function that loads (or 'requests') our repo data.
     Pass in some view function as a higher order callback, so our repos
     will render after the data is loaded. */
 
-});
+  reposObj.requestRepos(repoView.renderRepos);
+
+  module.repoView = repoView;
+})(window);
